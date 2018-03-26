774fc92a4eb2e557732cc2521bbec378007617bf
Leander Claes <claes@emt.uni-paderborn.de>
2018-03-26 17:54:41 +0200

    Add function to list connected visa devices.

--- keysightosc/oscilloscope.py
+++ keysightosc/oscilloscope.py
@@ -1,1 +1,8 @@
-"""Main module."""
+import visa as vi
+
+
+def list_connected_devices():
+    """List all connected VISA device addresses."""
+    rm = vi.ResourceManager()
+    resources = rm.list_resources()
+    return resources
